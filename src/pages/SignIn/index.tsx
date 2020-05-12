afda4d71616767e479d965ddfcfb06e46aef9ee4
Matheus Frauches <frauches.27@gmail.com>
2020-05-11 21:14:07 -0300

    Add custom Input and Button components to SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { Background, Container, Content } from './styles';
+
+import Input from '../../components/Input/';
+import Button from '../../components/Button/';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -11,10 +14,11 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <button type="submit">Entrar</button>
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
